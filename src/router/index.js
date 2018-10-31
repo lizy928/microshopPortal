398222f020c8d49f99e1d5e55e00093177971f64
lizhengyuan <lzy928>
2018-10-31 20:33:48 +0800

    前段代码构建

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+
+import world from '@/view/world'
+
+import Login from '@/view/Login'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/world',
+      name: 'world',
+      component: world
+    },
+    {
+      path: 'login',
+      name: 'login',
+      component: Login
+    }
+
+  ],
+  mode: "history"//干掉地址栏里边的#号键
+})
